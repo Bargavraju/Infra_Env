f79f7ce43bf806f06abf17d46fc08799d6f0519c
Bargaraju <bargav1014@gmail.com>
2026-02-12 13:44:24 +0530

    Modules

---
+++ modules/web_app/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_service_plan" "plan" {
+  name                = var.plan_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+
+    app_command_line = "python main.py"
+  }
+
+  app_settings = {
+    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
+  }
+}
